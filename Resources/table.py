e0dd09321ca2cde10f2fb134f93f86d14f794646
Mychele Larson <110074895+mychele-larson@users.noreply.github.com>
2023-01-26 19:33:02 -0600

    more

--- Resources/table.py
+++
@@ -1,9 +0,0 @@
-# Python program to convert
-# CSV to HTML Table
-import pandas as pd
-# to read csv file named "samplee"
-table = pd.read_csv("Webpage-Challenge/Resources/cities.csv")
-# to save as html file
-# named as "Table"
-table.to_html("Webpage-Challenge/data.html", index=False)
-
